7684a3fb0bea26fd72e1be0c1a8043efebedada3
ColeBurch <coleburch14@gmail.com>
2019-04-24 19:42:08 +0000

    completed exercises 3 and 4

---
+++ the_basics/ex3.rb
@@ -0,0 +1,5 @@
+Movies = {:I_Am_Legend => '2007', :Mountain => '2017', :The_Art_of_Flight => '2011'}
+
+puts Movies[:I_Am_Legend]
+puts Movies[:Mountain]
+puts Movies[:The_Art_of_Flight]
---
+++ the_basics/ex4.rb
@@ -0,0 +1,5 @@
+Movies = [2007, 2017, 2011]
+
+puts Movies[0]
+puts Movies[1]
+puts Movies[2]
